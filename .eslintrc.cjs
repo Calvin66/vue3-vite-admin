f5ff263e5877231e9c3178802c933c692883a07c
Lijianguo19 <627935@ky-tech.com.cn>
2023-04-13 17:23:30 +0800

    feat:配置eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,21 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  env: {
+    browser: true,
+    node: true,
+    es6: true
+  },
+  globals: {
+    // script setup
+    defineProps: 'readonly',
+    defineEmits: 'readonly',
+    defineExpose: 'readonly',
+    withDefaults: 'readonly',
+    // unplugin-vue-define-options
+    defineOptions: 'readonly'
+  },
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
